103c9b18d62042bb55841dc11a764e38a586f160
margaritabjerrum <margarita.juskyte@gmail.com>
2023-02-14 17:08:30 +0200

    feat: single bike page created

--- client/src/pages/bike-page/bike-card/bike-card.tsx
+++ client/src/pages/bike-page/bike-card/bike-card.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 import Img from 'components/layout/ui/img';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import * as Styled from './styled';
+import Header from './header';
 
 type BikeCardProps = BikeModel;
 
@@ -18,19 +24,42 @@ const BikeCard: React.FC<BikeCardProps> = ({
   },
   images,
 }) => (
-  <Stack>
-    <Img src={images[0]} alt="" sx={{ aspectRatio: '1.42', width: 1 }} />
-    <Typography>{id}</Typography>
-    <Typography>{brand}</Typography>
-    <Typography>{model}</Typography>
-    <Typography>{year}</Typography>
-    <Typography>{price}</Typography>
-    <Typography>{engine}</Typography>
-    <Typography>{power}</Typography>
-    <Typography>{seatHeight}</Typography>
-    <Typography>{weight}</Typography>
-
-  </Stack>
+  <>
+    <Header>{`${brand} - ${model}`}</Header>
+    <Grid container width="auto" spacing={2} alignItems="center">
+      <Grid item xs={12} md={6}>
+        <Swiper navigation modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <Img src={images[0]} alt="" sx={{ aspectRatio: '1.42', width: 1 }} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={images[1]} alt="" sx={{ aspectRatio: '1.42', width: 1 }} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Img src={images[2]} alt="" sx={{ aspectRatio: '1.42', width: 1 }} />
+          </SwiperSlide>
+        </Swiper>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Styled.ContentWrapper>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h1"
+            color="primary.dark"
+          >
+            At a glance
+          </Typography>
+          <Typography>{`Year: ${year}`}</Typography>
+          <Typography>{`Price: ${price.toFixed(2)}€`}</Typography>
+          <Typography>{`Engine: ${engine}`}</Typography>
+          <Typography>{`Power: ${power}`}</Typography>
+          <Typography>{`Seat Height: ${seatHeight}`}</Typography>
+          <Typography>{`Weight: ${weight}`}</Typography>
+        </Styled.ContentWrapper>
+      </Grid>
+    </Grid>
+  </>
 );
 
 export default BikeCard;
---
+++ client/src/pages/bike-page/bike-card/styled.ts
@@ -0,0 +1,8 @@
+import { Stack, styled } from '@mui/material';
+
+export const ContentWrapper = styled(Stack)(({ theme }) => ({
+  flexGrow: 1,
+  width: '100%',
+  padding: theme.spacing(1, 2),
+  alignItems: 'center',
+}));
